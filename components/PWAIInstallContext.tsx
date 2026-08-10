9fbd3512e2f454565d7286b0b168667e63820f36
Qrypton4 <remi.lalypro@gmail.com>
2026-08-10 09:53:55 +0200

    Remove unnecessary line in PWAIInstallContext.tsx

--- components/PWAIInstallContext.tsx
+++ components/PWAIInstallContext.tsx
@@ -6,7 +6,6 @@ type BeforeInstallPromptEvent = Event & {
   prompt: () => Promise<void>;
   userChoice: Promise<{ outcome: "accepted" | "dismissed"; platform: string }>;
 };
-
 type PWAInstallContextValue = {
   canInstall: boolean;
   isIOS: boolean;
