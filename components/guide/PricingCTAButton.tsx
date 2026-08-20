9b6fe7af748159d3f1cbf310badbb9329e27b672
Qrypton4 <remi.lalypro@gmail.com>
2026-08-20 18:19:44 +0200

    Make href prop optional for PricingCTAButton

--- components/guide/PricingCTAButton.tsx
+++ components/guide/PricingCTAButton.tsx
@@ -2,12 +2,12 @@
 
 import { useRouter } from "next/navigation";
 
-export default function PricingCTAButton() {
+export default function PricingCTAButton({ href = "/tarifs" }: { href?: string }) {
   const router = useRouter();
 
   return (
     <button
-      onClick={() => router.push("/tarifs")}
+      onClick={() => router.push(href)}
       className="inline-block px-6 py-3.5 rounded-lg text-[14.5px] font-semibold bg-blue text-white hover:bg-[#5279ff] transition"
     >
       Voir les offres
