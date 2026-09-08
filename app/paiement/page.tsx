88172c3d439ccd7a5a7279208b85204db5d3d25e
Qrypton4 <remi.lalypro@gmail.com>
2026-09-08 20:20:43 +0200

    Fix user parameter in isSalesOpen check

--- app/paiement/page.tsx
+++ app/paiement/page.tsx
@@ -17,7 +17,7 @@ export default async function Paiement({
   const supabase = createClient();
   const { data: { user } } = await supabase.auth.getUser();
 
-  if (!(await isSalesOpen(user?.email))) {
+  if (!(await isSalesOpen (user))) {
     redirect("/tarifs/fonds-propres");
   }
 
